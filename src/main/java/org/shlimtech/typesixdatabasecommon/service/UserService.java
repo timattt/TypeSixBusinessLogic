ef8ef5dddafc90c9a0adcb77e480e0126e5dfcf9
timattt <timattt@gmail.com>
2024-01-24 15:32:41 +0300

    get all users method

--- src/main/java/org/shlimtech/typesixdatabasecommon/service/UserService.java
+++ src/main/java/org/shlimtech/typesixdatabasecommon/service/UserService.java
@@ -8,6 +8,9 @@ import org.shlimtech.typesixdatabasecommon.repository.UserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @RequiredArgsConstructor
 public class UserService {
@@ -49,6 +52,10 @@ public class UserService {
     public UserDTO loadUser(int id) {
         User user = userRepository.getReferenceById(id);
         return modelMapper.map(user, UserDTO.class);
+    }
+
+    public List<UserDTO> getAllUsers() {
+        return userRepository.findAll().stream().map(user -> modelMapper.map(user, UserDTO.class)).collect(Collectors.toList());
     }
 
 }
